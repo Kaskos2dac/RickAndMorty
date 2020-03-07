cffc6799d79cd46f50ed9b0c8c4f6c535cb7a66c
DanielAcero <daniacerocotacio@gmail.com>
2020-03-07 18:39:17 -0500

    creating components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardCharacterComponent } from './card-character/card-character.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardCharacterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-character/card-character.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardCharacterComponent } from './card-character.component';
+
+describe('CardCharacterComponent', () => {
+  let component: CardCharacterComponent;
+  let fixture: ComponentFixture<CardCharacterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardCharacterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardCharacterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
